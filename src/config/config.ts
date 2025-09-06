def1930f671a18bb9ad8743e431384ad8b272e37
Umolemu <lambvries@gmail.com>
2025-09-06 14:26:58 +0200

    Change url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,2 @@
-const API_BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:3000"
-    : "https://task-manager-be-6vuq.onrender.com";
-
+const API_BASE_URL = "https://task-manager-be-6vuq.onrender.com";
 export default API_BASE_URL;
